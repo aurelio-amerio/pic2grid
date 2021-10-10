2209b846cebed0a2cb1394e958d8edb65c925a38
Aurelio Amerio <aurelio.amerio@edu.unito.it>
2021-10-10 16:11:02 +0200

    test output

--- pic2grid.py
+++ pic2grid.py
@@ -9,16 +9,16 @@ col_dpi=267
 width_A4 = 595/72
 height_A4 = 842/72
 
-ncol = 3
-nrows = 8
+ncols = 2
+nrows = 6
 
 input_folder = "sample_images"
 output_folder = "output"
 
 
 #%%
-def make_single_gird(files, nrows, ncol, offset=0, index=0, hspace=0.01):
-    fig, axes = plt.subplots(nrows,ncol, figsize=(width_A4, height_A4)) # righe, colonne, (colonne, righe)
+def make_single_gird(files, nrows, ncols, offset=0, index=0, hspace=0.01):
+    fig, axes = plt.subplots(nrows,ncols, figsize=(width_A4, height_A4)) # righe, colonne, (colonne, righe)
     for ax, img, i in zip(axes.transpose().reshape(-1), files, np.arange(0, len(files))):
         ax.axis('off')
         sample=plt.imread(img)
@@ -31,21 +31,21 @@ def make_single_gird(files, nrows, ncol, offset=0, index=0, hspace=0.01):
 
     # fig.set_tight_layout(True)
     plt.subplots_adjust(hspace=hspace)
-    plt.savefig(f"{output_folder}/output_{index}.png", dpi=col_dpi*ncol, bbox_inches = "tight")
+    plt.savefig(f"{output_folder}/output_{ncols}x{nrows}_{index}.png", dpi=col_dpi*ncols, bbox_inches = "tight")
     plt.close()
 
-def process_input(nrows, ncol):
+def process_input(nrows, ncols):
     files = glob(f"{input_folder}/*.png")
-    step = nrows*ncol
+    step = nrows*ncols
     try:
-        assert len(files) % (nrows*ncol) == 0
+        assert len(files) % (nrows*ncols) == 0
     except:
-        print(f"The number of input files must be a multiple of {nrows*ncol}, {len(files)} provided.")
+        print(f"The number of input files must be a multiple of {nrows*ncols}, {len(files)} provided.")
         return
     nmin_array = np.arange(0, len(files), step=step)
     for i, nmin in enumerate(tqdm(nmin_array)):
-        make_single_gird(files[nmin:(nmin+step)], nrows, ncol, offset=i*step, index=i)
+        make_single_gird(files[nmin:(nmin+step)], nrows, ncols, offset=i*step, index=i)
 
 # %%
-process_input(6, 2)
+process_input(nrows, ncols)
 # %%
